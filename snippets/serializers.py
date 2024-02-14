ab1435bbf3553098e3f98e519a5bd5514c0f828d
Collins Mmasaba <mmasabacollins9@gmail.com>
2024-02-14 22:00:38 +0300

    Using hyperlinks for object relatiionships

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -2,15 +2,20 @@ from rest_framework import serializers
 from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 from users.models import CustomUserModel as User
 
-class SnippetSerializer(serializers.ModelSerializer):
-    owner = serializers.ReadOnlyField(source='owner.username')
-    class Meta:
-        model = Snippet
-        fields = ['id', 'title', 'code', 'linenos', 'language', 'style', 'owner']
-
-
-class UserSerializer(serializers.ModelSerializer):
-    snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    snippets = serializers.HyperlinkedRelatedField(many=True, view_name='snippet-detail', read_only=True)
+    # To bypass the lookup_field error, explicitly define the url this way
+    url = serializers.HyperlinkedIdentityField(view_name='user-detail', format='html')
+    
     class Meta:
         model = User
-        fields = ['id', 'username', 'snippets']
+        fields = ['url', 'id', 'username', 'snippets']
+
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    highlight = serializers.HyperlinkedIdentityField(view_name='snippet-highlight', format='html')
+
+    class Meta:
+        model = Snippet
+        fields = ['url' 'id', 'highlight', 'title', 'code', 
+                  'linenos', 'language', 'style', 'owner']
