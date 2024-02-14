1af422dce9a26a9d18eb20cc760431f151c7a500
Collins Mmasaba <mmasabacollins9@gmail.com>
2024-02-14 16:49:39 +0300

    Views for user endpoints

--- snippets/views.py
+++ snippets/views.py
@@ -1,6 +1,10 @@
 from snippets.models import Snippet
-from snippets.serializers import SnippetSerializer
+from snippets.serializers import SnippetSerializer, UserSerializer
 from rest_framework import generics
+from rest_framework import permissions
+from snippets.permissions import IsOwnerOrReadOnly
+
+from users.models import CustomUserModel as User
 
 # using mixed-in generic views to write concise representations
 class SnippetList(generics.ListCreateAPIView):
@@ -9,6 +13,12 @@ class SnippetList(generics.ListCreateAPIView):
     """
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly]
+
+    def perform_create(self, serializer):
+        """Associate the snippet with the user who created it."""
+        serializer.save(owner=self.request.user)
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     """
@@ -16,3 +26,19 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly]
+
+class UserList(generics.ListCreateAPIView):
+    """
+    List all users, or create a new user.
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Retrieve, update or delete a user instance.
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer   
